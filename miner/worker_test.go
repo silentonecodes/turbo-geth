8d10e36f20be2a49b8b1f55adaf7d2e52c2cd4b6
Igor Mandrigin <i@mandrigin.ru>
2020-04-19 14:37:10 +0300

    post-rebase: fix complile error in miner test

--- miner/worker_test.go
+++ miner/worker_test.go
@@ -330,7 +330,7 @@ func testGenerateBlockAndImport(t *testing.T, testCase *testCase, isClique bool)
 		select {
 		case ev := <-sub.Chan():
 			block := ev.Data.(core.NewMinedBlockEvent).Block
-			if _, err := chain.InsertChain([]*types.Block{block}); err != nil {
+			if _, err := chain.InsertChain(context.Background(), []*types.Block{block}); err != nil {
 				t.Fatalf("failed to insert new mined block %d: %v", block.NumberU64(), err)
 			}
 		case <-time.After(3 * time.Second): // Worker needs 1s to include new changes.
